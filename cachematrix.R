c4727b5cce5a3de601ef227d23291c068341560c
pal27oct <palani27oct@gmail.com>
2015-04-26 12:17:47 +0530

    assignment code submission
    This update has the code for the Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-## test
+## Script for Programming Assignment 2
+## Caching inverse of matrix
+## Provide singular matrix to test and call $reset(anyvalue) to rest m
 
-## Write a short comment describing this function
+## this method stores the value in cache
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+#call this method to reset m
+        reset <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(value) m <<- value
 
+        getinverse <- function() m
+        list(reset = reset , get = get,
+             setinverse = setinverse ,
+             getinverse = getinverse )
 }
 
-
-## Write a short comment describing this function
+## this method computes inverse of a singular matrix
+## stores the inverse in cache, upon consecutive call values are fetched from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data) %*% data
+        x$setinverse(m)
+        m
 }
